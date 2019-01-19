a05c0a85492a4fafc37a6083529665f75f5a2115
Nico <nicolas.romero.f@usach.cl>
2019-01-18 22:40:42 -0300

    listo

--- lab4.r
+++ lab4.r
@@ -1,7 +1,9 @@
 require('e1071')
+library('caret')
 
-#path = "F:/Google Drive/USACH/Nivel 8/Analisis de datos/lab2/hepatitis.data"
-path = "~/Documentos/AnalisisDatosLab4/hepatitis.data"
+
+path = "F:/Google Drive/USACH/Nivel 8/Analisis de datos/lab3/hepatitis.data"
+#path = "~/Documentos/AnalisisDatosLab4/hepatitis.data"
 hepatitis <- read.table(path,sep=",", na.strings = c("?"))
 
 names <- c("CLASS","AGE","SEX","STEROID","ANTIVIRALS","FATIGUE","MALAISE",
@@ -12,6 +14,34 @@ names <- c("CLASS","AGE","SEX","STEROID","ANTIVIRALS","FATIGUE","MALAISE",
 colnames(hepatitis) <- names
 
 hepatitis.without.na <- na.omit(hepatitis)
-data = hepatitis.without.na
+data = as.data.frame(hepatitis.without.na)
+data[names] <- lapply(data[names], factor)
+
 #data = select(hepatitis.without.na,-CLASS)
-naiveBayes(CLASS ~ ., data)
+print("factor")
+set.seed(2019)
+
+trainIndex=createDataPartition(data$CLASS, p=0.7)$Resample1
+train=data[trainIndex, ]
+test=data[-trainIndex, ]
+
+## check the balance
+print(table(data$CLASS))
+print(table(train$CLASS))
+
+print("fin tabla")
+model = naiveBayes(CLASS ~ ., train)
+
+trainPred=predict(model, train)
+trainTable=table(train$CLASS, trainPred)
+testPred=predict(model,test)
+testTable=table(test$CLASS, testPred)
+print("predict listo")
+trainAcc=(trainTable[1,1]+trainTable[2,2])/sum(trainTable)
+testAcc=(testTable[1,1]+testTable[2,2])/sum(testTable)
+message("Contingency Table for Training Data")
+print(trainTable)
+message("Contingency Table for Test Data")
+print(testTable)
+message("Accuracy")
+print(round(cbind(trainAccuracy=trainAcc, testAccuracy=testAcc),3))
