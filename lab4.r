3973cf7820913727a2963b4af7dcbddd1d58c411
Nico <nicolas.romero.f@usach.cl>
2019-01-19 00:23:19 -0300

    listo

--- lab4.r
+++ lab4.r
@@ -14,7 +14,30 @@ names <- c("CLASS","AGE","SEX","STEROID","ANTIVIRALS","FATIGUE","MALAISE",
 colnames(hepatitis) <- names
 
 hepatitis.without.na <- na.omit(hepatitis)
-data = as.data.frame(hepatitis.without.na)
+data = hepatitis.without.na
+
+data[data["AGE"] <= 30,"AGE"] <- 0
+data[data["AGE"] > 30,"AGE"] <- 1
+
+data[data["BILIRUBIN"] < 0.1,"BILIRUBIN"] <- 0
+data[data["BILIRUBIN"] > 1.2,"BILIRUBIN"] <- 0
+data[data["BILIRUBIN"] != 0,"BILIRUBIN"] <- 1
+
+data[data["ALK_PHOSPHATE"] < 44,"ALK_PHOSPHATE"] <- 0
+data[data["ALK_PHOSPHATE"] > 147,"ALK_PHOSPHATE"] <- 0
+data[data["ALK_PHOSPHATE"] != 0,"ALK_PHOSPHATE"] <- 1
+
+data[data["SGOT"] < 5,"SGOT"] <- 0
+data[data["SGOT"] > 40,"SGOT"] <- 0
+data[data["SGOT"] != 0,"SGOT"] <- 1
+
+data[data["ALBUMIN"] < 3.4,"ALBUMIN"] <- 0
+data[data["ALBUMIN"] > 5.4,"ALBUMIN"] <- 0
+data[data["ALBUMIN"] != 0,"ALBUMIN"] <- 1
+
+data[data["PROTIME"] <= 60,"PROTIME"] <- 0
+data[data["PROTIME"] > 60,"PROTIME"] <- 1
+
 data[names] <- lapply(data[names], factor)
 
 #data = select(hepatitis.without.na,-CLASS)
@@ -32,6 +55,8 @@ print(table(train$CLASS))
 print("fin tabla")
 model = naiveBayes(CLASS ~ ., train)
 
+print(model)
+
 trainPred=predict(model, train)
 trainTable=table(train$CLASS, trainPred)
 testPred=predict(model,test)
@@ -44,4 +69,5 @@ print(trainTable)
 message("Contingency Table for Test Data")
 print(testTable)
 message("Accuracy")
+
 print(round(cbind(trainAccuracy=trainAcc, testAccuracy=testAcc),3))
